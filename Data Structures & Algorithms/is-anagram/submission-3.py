29079e9b0911a778476fb1ce93172d073d6fdba7
Israa Elhouch <issrach235@gmail.com>
2026-09-04 11:04:59 +0100

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,24 @@
+"""class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        seen = set()
+        if len(s)== len(t):
+            for letter in s:
+                if letter not in seen:
+                    seen.add(letter)
+                    if s.count(letter)!= t.count(letter):
+                        return False
+            return True
+                
+        else:
+            return False
+"""
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!= len(t):
+            return False
+        return sorted(s)==sorted(t)
+
+"""class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)"""
